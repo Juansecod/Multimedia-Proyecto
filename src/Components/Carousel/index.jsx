e9eabb56b7552a20065c86de102d22bacfdf86a2
Juan Sebastian <juan.sebastianri28@gmail.com>
2025-05-29 20:37:19 +0000

    Add content to foxey page

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "./Carousel.css";
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
--- src/Pages/FoxeyPage/index.jsx
+++ src/Pages/FoxeyPage/index.jsx
@@ -1,18 +1,27 @@
 import BarLeft from '../../Components/BarLeft';
 import BarRight from '../../Components/BarRight';
 import NavBar from '../../Components/NavBar';
+import BarFooter from '../../Components/BarFooter';
+
+import funFactFoxeyImage from '@/assets/Foxey-fun-fact.jpg';
+import foxeyCarrusel1 from '@/assets/foxey-carrusel-1.jpg';
+import foxeyCarrusel2 from '@/assets/foxey-carrusel-2.jpg';
+import foxeyCarrusel3 from '@/assets/foxey-carrusel-3.jpg';
+import foxeyCarrusel4 from '@/assets/foxey-carrusel-4.jpg';
+import foxeyCarrusel5 from '@/assets/foxey-carrusel-5.jpg';
+
 
 import foxey from '../../assets/foxey-logo.png';
 import lhod from '../../assets/lhod-logo.png';
 import bantu from '../../assets/bantu-logo.png';
 import valhala from '../../assets/valhala-logo.png';
-import BarFooter from '../../Components/BarFooter';
+
 
 function FoxeyPage() {
-  const images = [foxey, lhod, bantu, valhala];
+  const images = [foxeyCarrusel1, foxeyCarrusel2, foxeyCarrusel3, foxeyCarrusel4, foxeyCarrusel5];
 
   const text = <p style={{fontSize: "16px"}}>Bienvenido al lugar donde vas a encontrar tu <span className='new-rocker-font' style={{fontSize: "20px"}}>“Beer Spirit”</span>.
-Aquí la cerveza no se toma, se vive. Tienen desde una pola negra intensa como tu alma, hasta una dorada brillante que parece embotellar el sol. Acá podras parchar con tus amigos, hablar de la vida, ver buen deporte y dejarte llevar por el mejor rock. <br />Recuerda que aqui... <span className='rock-salt-font'>"Smells like beer spirit”</span></p>;
+Aquí la cerveza no se toma, se vive. Tienen desde una pola negra intensa como tu alma, hasta una dorada brillante que parece embotellar el sol. Acá podras parchar con tus amigos, hablar de la vida, ver buen deporte y dejarte llevar por el mejor rock. <br /><br />Recuerda que aqui... <span className='rock-salt-font'>"Smells like beer spirit”</span></p>;
 
   const spotifyEmbed = `
     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4CeeEOM32jQcH3eN9Q2dGj?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
@@ -44,8 +53,10 @@ Aquí la cerveza no se toma, se vive. Tienen desde una pola negra intensa como t
       { image: valhala, title: 'Bebida 3', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbitristique senectus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbitristique senectus." },
     ],
     factTitle: "Sabías que",
-    factText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbitristique senectus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbitristique senectus.',
-    cardImage: bantu,
+    factText: (<p>
+      En Foxey, cada 4 de mayo se celebra el <span className='new-rocker-font' style={{fontSize: "20px"}}>Día de Star Wars</span> con un evento fuera de este mundo. En honor al famoso <span className='new-rocker-font' style={{fontSize: "20px"}}>“May the 4th be with you”</span>, se organiza un concurso cervecero en el que participan creaciones inspiradas en la saga, como la rebelde <span className='new-rocker-font' style={{fontSize: "20px"}}>Andor</span> o la poderosa <span className='new-rocker-font' style={{fontSize: "20px"}}>Anakin</span>, actual campeona. Así que ya sabes: agenda el próximo 4 de mayo, ven con tu mejor traje Jedi (o Sith), prueba estas cervezas intergalácticas y participa en un evento donde la Fuerza y la cebada, están contigo.
+    </p>),
+    cardImage: funFactFoxeyImage,
   };
 
   const barFooterProps = {
